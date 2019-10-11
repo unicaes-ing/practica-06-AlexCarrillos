b9dd79fdc255f7ac4d098d4e5a2cd82bc329db45
AlexCarrillos <54732270+AlexCarrillos@users.noreply.github.com>
2019-10-10 23:35:39 -0600

    Add files via upload

---
+++ Guia6/Guia6/Class1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guia6
+{
+    class Class1
+    {
+        public class Validacion
+        {
+            public static void Validar(string mensaje, out int num)
+            {
+                bool condicion;
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine(mensaje);
+                    condicion = int.TryParse(Console.ReadLine(), out num);
+                    if (condicion == false || num < 0)
+                    {
+                        Console.WriteLine("Valor no válido. Ingresar de nuevo.");
+                        Console.WriteLine("Presione <Enter> para continuar...");
+                        Console.ReadKey();
+                    }
+                } while (condicion == false || num < 0);
+            }
+
+            public static void Validar(string mensaje, double num = 0)
+            {
+                bool condicion;
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine(mensaje);
+                    condicion = double.TryParse(Console.ReadLine(), out num);
+                    if (condicion == false || num < 0)
+                    {
+                        Console.WriteLine("Valor no válido. Ingresar de nuevo.");
+                        Console.WriteLine("Presione <Enter> para continuar...");
+                        Console.ReadKey();
+                    }
+                } while (condicion == false || num < 0);
+            }
+
+        }
+
+
+        public class Ejercicio1 : Validacion
+        {
+            public static void Tabla(int num)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(num + " * " + i + " = " + num * i);
+                }
+            }
+
+
+            // Funcion2
+            public static void Tabla(int num, ConsoleColor color)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.ForegroundColor = color;
+                    Console.WriteLine(num + " * " + i + " = " + num * i);
+                }
+            }
+
+        }
+
+        public class Ejercicio2 : Validacion
+        {
+
+
+
+            #region Funcion1
+            public static void Dibujo(int alto, int ancho)
+            {
+                for (int i = 0; i < alto; i++)
+                {
+                    Console.WriteLine(" ");
+                    Console.Write(" ");
+                    for (int j = 0; j < ancho; j++)
+                    {
+                        Console.Write("*");
+                        Console.Write(" ");
+                    }
+                }
+            }
+            #endregion
+
+            #region Sobrecarga1
+            public static void Dibujo(int alto, int ancho, char caracter)
+            {
+                for (int i = 0; i < alto; i++)
+                {
+                    Console.WriteLine(" ");
+                    Console.Write(" ");
+                    for (int j = 0; j < ancho; j++)
+                    {
+                        Console.Write(caracter);
+                        Console.Write(" ");
+                    }
+                }
+            }
+            #endregion
+
+            #region Sobrecarga2
+            public static void Dibujo(int alto, int ancho, char caracter, int x, int y)
+            {
+                for (int i = 0; i < alto; i++)
+                {
+                    Console.WriteLine(" ");
+                    Console.Write(" ");
+                    Console.SetCursorPosition(x, y++);
+                    for (int j = 0; j < ancho; j++)
+                    {
+                        Console.Write(caracter);
+                        Console.Write(" ");
+                    }
+                }
+            }
+            #endregion
+        }
+
+        public class Ejercicio3 : Validacion
+        {
+
+
+            #region Funcion
+            public static int NumElevado(int bases, int exponente)
+            {
+                if (exponente < 1)
+                {
+                    return 1;
+                }
+                return bases * NumElevado(bases, exponente - 1);
+            }
+            #endregion
+        }
+
+        public class Ejercicio4
+        {
+
+
+            public static void DecimalaBinario(int num)
+            {
+                if (num < 2)
+                {
+                    Console.Write(num);
+                }
+                else
+                {
+                    DecimalaBinario(num / 2);
+                    Console.Write(num % 2);
+                }
+            }
+        }
+    }
+}
